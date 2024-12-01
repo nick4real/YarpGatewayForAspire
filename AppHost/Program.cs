5ce8ada0cd027a3c44762e413c31e109ebb8ba7f
nick4real <niko.wsnh@gmail.com>
2024-12-01 02:54:39 +0100

    Old project remove

--- AppHost/Program.cs
+++
@@ -1,7 +0,0 @@
-var builder = DistributedApplication.CreateBuilder(args);
-
-builder.AddProject<Projects.User_API>("user-api");
-
-builder.AddProject<Projects.Product_API>("product-api");
-
-builder.Build().Run();
